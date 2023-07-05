749200cae82391b6cf87b60d73df54340baa939f
tjtoo04 <ttjiat@gmail.com>
2023-07-05 13:09:16 +0800

    added some classes

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-#hello
-#yhhhh
+class User:
+    def __init__(self, ID, Password, Name, Age):
+        self.ID = ID
+        self.Password = Password
+        self.Name = Name
+        self.Age = Age
+
+
+class Admin:
+    pass
+
+class Receptionist:
+    pass
+
+class Tutor:
+    pass
+
+class Student:
+    pass
+
+def login():
+    pass
+
+def register():
+    pass
